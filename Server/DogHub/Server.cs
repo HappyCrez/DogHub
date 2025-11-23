f020e6b57d9a2ec7ac9538faeeb308a8cdc0416c
lexcivis <gorbunov-2004@bk.ru>
2025-11-23 17:59:57 +0700

    Comments & correct disconnect

--- Server/DogHub/Server.cs
+++ Server/DogHub/Server.cs
@@ -4,20 +4,29 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
+public interface ICloseConnection
+{
+    void CloseConnection(int clientId);
+}
+
 /// <summary>
 /// HTTP-сервер DogHub.
 /// Передает подключения на обработку ClientHandler
 /// </summary>
-class Server
+class Server : ICloseConnection
 {
     private const int Port = 5055;
     private static int clientId = 0;
     private Dictionary<int, ClientHandler> client = new Dictionary<int, ClientHandler>();
 
-    public DataBaseModel dbModel { get; set; }
-    public SQLCommandManager sqlCM { get; set; }
+    private DataBaseModel dbModel { get; set; }
+    private SQLCommandManager sqlCM { get; set; }
 
-
+    /// <summary>
+    /// Слушает подключения и передает их на обработку объекту ClientHandler
+    /// </summary>
+    /// <param name="dbModel">Объект выполняющий запросы к БД</param>
+    /// <param name="sqlCM">Command Manager хранит select команды</param>
     public Server(DataBaseModel dbModel, SQLCommandManager sqlCM)
     {
         this.dbModel = dbModel;
@@ -39,7 +48,7 @@ class Server
 
                 // сохраняем пару уникальный id клиента и ссылку на обрабатывающий подключение объект
                 clientId++;
-                client.Add(clientId, new ClientHandler(dbModel, sqlCM));
+                client.Add(clientId, new ClientHandler(clientId, dbModel, sqlCM, this));
 
                 // Для каждого клиента запускаем отдельный поток обработки.
                 Thread clientThread = new Thread(client[clientId].Handle);
@@ -53,6 +62,18 @@ class Server
         finally
         {
             listener?.Stop();
+        }
+    }
+
+    /// <summary>
+    /// Удаляет объект после отключения клиента
+    /// </summary>
+    /// <param name="clientId">Идентификатор клиента</param>
+    public void CloseConnection(int clientId)
+    {
+        if (client.ContainsKey(clientId))
+        {
+            client.Remove(clientId);
         }
     }
 }
